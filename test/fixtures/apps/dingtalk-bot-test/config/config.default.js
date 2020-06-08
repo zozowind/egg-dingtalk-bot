180011b1297e7d801c9ee28056eac02691698726
Marco Chen <zozowind@gmail.com>
2020-06-08 18:34:07 +0800

    添加配置文件

---
+++ test/fixtures/apps/dingtalk-bot-test/config/config.default.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.keys = '123456';
+
+exports.dingtalkBot = {
+  name: '机器人名字',
+  accessToken: 'use_your_own_token',
+  secret: 'use_your_own_secret_if_choose_sign_check',
+};
---
+++ test/fixtures/apps/dingtalk-multiple-test/config/config.default.js
@@ -0,0 +1,13 @@
+'use strict';
+
+exports.keys = '123456';
+
+exports.dingtalkBot = {
+  robots: {
+    bot1: {
+      name: '机器人名字',
+      accessToken: 'use_your_own_token',
+      secret: 'use_your_own_secret_if_choose_sign_check',
+    },
+  },
+};
